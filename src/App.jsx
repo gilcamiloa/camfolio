497c399a2a60b5388c07ae6f4e8916133e4872a1
gilcamiloa <cgilv6@gmail.com>
2021-11-27 23:11:54 -0500

    sections updated

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.scss'
+import Navbar from './components/navbar/Navbar'
+import Intro from './components/intro/Intro'
+import About from './components/about/About'
+import Projects from './components/projects/Projects'
+import Contact from './components/contact/Contact'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <div className="sections" >
+        <About/>
+        <Intro/>
+        <Projects/>
+        <Contact/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/about/about.jsx
@@ -0,0 +1,13 @@
+import './about.scss'
+
+export default function about() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem laborum rerum nihil recusandae asperiores. Reprehenderit nisi quibusdam blanditiis illo tenetur cumque optio sed, nobis deserunt, suscipit hic atque consectetur magnam!</p>
