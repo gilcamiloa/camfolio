62921877d15c7afc390f8bf5669d246773567d27
gilcamiloa <cgilv6@gmail.com>
2021-11-29 22:58:15 -0500

    more navbar info included

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -3,7 +3,14 @@ import './navbar.scss'
 export default function Navbar() {
   return (
     <div className='navbar'>
-
+      <div className="d-flex align-items-center">
+        <div>
+          CAM
+        </div>
+        <div>
+          Nav bar things included here...
+        </div>
+      </div>
     </div>
   )
 }
