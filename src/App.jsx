76ddd63290112da18625baa7b517a0a2d078402c
gilcamiloa <cgilv6@gmail.com>
2021-12-05 16:08:24 -0500

    created sidebar menu

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import Intro from './components/intro/Intro'
 import About from './components/about/About'
 import Projects from './components/projects/Projects'
 import Contact from './components/contact/Contact'
+import { useState } from "react";
+import Menu from "./components/menu/Menu";
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false)
   return (
     <div className="app">
-      <Navbar/>
+      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro/>
         <About/>
