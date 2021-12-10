2e45fa5b89d726cd2e1069d4afb7c3ddefcc4b78
gilcamiloa <cgilv6@gmail.com>
2021-12-09 21:21:24 -0500

    insert source code for projects list and populate accordingly

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -21,6 +21,31 @@ export default function projects() {
             <h2>Is there any DATA in there?</h2>
             <h2>Just nod if you can he me</h2>
           </div>
+          return (
+          {/* <div className="portfolio" id="portfolio">
+            <h1>Portfolio</h1>
+            <ul>
+              {list.map((item) => (
+                <PortfolioList
+                  title={item.title}
+                  active={selected === item.id}
+                  setSelected={setSelected}
+                  id={item.id}
+                />
+              ))}
+            </ul>
+            <div className="container">
+              {data.map((d) => (
+                <div className="item">
+                  <img
+                    src={d.img}
+                    alt=""
+                  />
+                  <h3>{d.title}</h3>
+                </div>
+              ))}
+            </div>
+          </div> */}
         </div>
       </div>
     </div>
