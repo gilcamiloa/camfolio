f1d397462ff542d6611b135c60d4a6c8eed5800c
gilcamiloa <cgilv6@gmail.com>
2021-12-18 21:43:59 -0500

    added files and played around to optimize routes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,9 @@
 import './navbar.scss'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faBars } from '@fortawesome/free-solid-svg-icons'
+// import About from "../about/About.jsx"
 
-
-
-export default function Navbar({ menuOpen, setMenuOpen }) {
+export default function Navbar() {
   return (
     <div className='navbar'>
     {/* // <div className={'navbar' + (menuOpen && 'active')}> */}
@@ -16,9 +15,9 @@ export default function Navbar({ menuOpen, setMenuOpen }) {
           <div className="about">
             <a href="#about" className="logo">About</a>
           </div>
-          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+          {/* <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
             <a href="menu"><FontAwesomeIcon icon={faBars}/></a>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
