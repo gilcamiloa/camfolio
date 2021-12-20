968a0eb94937694b5f09d841b30ae7fa21961269
gilcamiloa <cgilv6@gmail.com>
2021-12-19 19:54:41 -0500

    issues with navbar link tag and rendering nav/footer

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,8 @@
 import './navbar.scss'
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faBars } from '@fortawesome/free-solid-svg-icons'
-// import About from "../about/About.jsx"
+import About from "../about/About.jsx"
+import { Link } from 'react-dom';
 
 export default function Navbar() {
   return (
@@ -13,7 +14,8 @@ export default function Navbar() {
         </div>
         <div className="right">
           <div className="about">
-            <a href="#about" className="logo">About</a>
+            {/* <a href="#about" className="logo">About</a> */}
+            <Link path="/about" element={ <About/> }></Link>
           </div>
           {/* <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
             <a href="menu"><FontAwesomeIcon icon={faBars}/></a>
