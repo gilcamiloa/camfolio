b18d3c1abe5005daa55ea17004895686405ab6aa
gilcamiloa <cgilv6@gmail.com>
2021-12-20 15:28:26 -0500

    up and running need slider to work upon click

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import './navbar.scss'
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faBars } from '@fortawesome/free-solid-svg-icons'
 // import About from "../about/About.jsx"
+// import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
@@ -9,11 +10,12 @@ export default function Navbar() {
     {/* // <div className={'navbar' + (menuOpen && 'active')}> */}
       <div className='wrapper'>
         <div className="left">
-          <a href="#intro" className="logo">CAM</a>
+          <a href="/" className="logo">CAM</a>
         </div>
         <div className="right">
           <div className="about">
-            <a href="#about" className="logo">About</a>
+            <a href="/about" className="logo">About</a>
+            {/* <Link to='/about'>About</Link> */}
           </div>
           {/* <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
             <a href="menu"><FontAwesomeIcon icon={faBars}/></a>
