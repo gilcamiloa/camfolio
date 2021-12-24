c81f617d4c992cb7a6bf18d1947aa333e7fa5f00
gilcamiloa <cgilv6@gmail.com>
2021-12-23 20:19:11 -0500

    xscrolling working

--- src/Sections.jsx
+++ src/Sections.jsx
@@ -5,23 +5,24 @@ import Skills from './components/skills/Skills'
 import Contact from './components/contact/Contact'
 
 function Sections() {
-  const xScroll = document.querySelector('.sectionsContainer');
+  // const xScroll = document.querySelector('.sectionsContainer');
 
-    xScroll.addEventListener("wheel", (evt) => {
-    evt.preventDefault();
-    xScroll.scrollLeft += evt.deltaY;
-    });
 
   return (
     <div className="sections">
       <div className="sectionsContainer">
-        <Intro exact path="/" />
-        <Projects path="/projects" />
-        <Skills path="/skills" />
-        <Contact path="/contact" />
+        <Intro exact path="/" className="sectionSlides"/>
+        <Projects path="/projects" className="sectionSlides"/>
+        <Skills path="/skills" className="sectionSlides"/>
+        <Contact path="/contact" className="sectionSlides"/>
       </div>
     </div>
   );
 }
+
+// document.querySelector('.sectionsContainer').addEventListener("wheel", (e) => {
+// e.preventDefault();
+// e.scrollLeft += e.deltaY;
+// });
 
 export default Sections;
