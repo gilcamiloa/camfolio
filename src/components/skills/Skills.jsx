e703bd417ba2feb5fc0fc8d85e5cf7e288afe30b
gilcamiloa <cgilv6@gmail.com>
2022-01-12 16:21:45 -0500

    progress bar on skills and projects title postioning

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,12 +1,12 @@
 import './skills.scss'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import { faLinkedin,
-          faGithub,
-          faJsSquare,
+import {  faJsSquare,
           faCss3Alt,
           faHtml5,
+          faReact,
+          faPython
          } from '@fortawesome/free-brands-svg-icons'
-import { faDatabase, faFile, faGem, faMailBulk} from '@fortawesome/free-solid-svg-icons'
+import { faDatabase, faGem } from '@fortawesome/free-solid-svg-icons'
 import { ProgressBar } from 'react-bootstrap'
 
 export default function Skills() {
@@ -15,19 +15,24 @@ export default function Skills() {
       <div id="skills">
         <div className="title">
           <h1>Skills</h1>
-          <div className="links container" >
-            <div id="progress_bar__container" className="">
-              <span>HTML&ensp;<FontAwesomeIcon icon={faHtml5}></FontAwesomeIcon></span>
-              <ProgressBar now="90" visuallyHidden variant="warning" />
-              <span>CSS&ensp;<FontAwesomeIcon icon={faCss3Alt}></FontAwesomeIcon></span>
-              <ProgressBar now="85" visuallyHidden variant="success" />
-              <span>JavaScript&ensp;<FontAwesomeIcon icon={faJsSquare}></FontAwesomeIcon></span>
-              <ProgressBar now="75" visuallyHidden variant="info" />
-              <span>Ruby - Ruby on Rails&ensp;<FontAwesomeIcon icon={faGem}></FontAwesomeIcon></span>
-              <ProgressBar now="85" visuallyHidden variant="danger" />
-              <span>SQL&ensp;<FontAwesomeIcon icon={faDatabase}></FontAwesomeIcon></span>
-              <ProgressBar now="70" visuallyHidden />
-            </div>
+        </div>
+        <br />
+        <div className="links container" >
+          <div id="progress_bar__container" className='items'>
+            <span>HTML&ensp;<FontAwesomeIcon icon={faHtml5}></FontAwesomeIcon></span>
+            <ProgressBar now="90" visuallyHidden variant="warning" />
+            <span>CSS&ensp;<FontAwesomeIcon icon={faCss3Alt}></FontAwesomeIcon></span>
+            <ProgressBar now="85" visuallyHidden variant="success" />
+            <span>JavaScript&ensp;<FontAwesomeIcon icon={faJsSquare}></FontAwesomeIcon></span>
+            <ProgressBar now="75" visuallyHidden variant="info" />
+            <span>React.js&ensp;<FontAwesomeIcon icon={faReact}></FontAwesomeIcon></span>
+            <ProgressBar now="75" visuallyHidden variant="info" />
+            <span>Python&ensp;<FontAwesomeIcon icon={faPython}></FontAwesomeIcon></span>
+            <ProgressBar now="30" visuallyHidden variant="info" />
+            <span>Ruby - Ruby on Rails&ensp;<FontAwesomeIcon icon={faGem}></FontAwesomeIcon></span>
+            <ProgressBar now="85" visuallyHidden variant="danger" />
+            <span>SQL&ensp;<FontAwesomeIcon icon={faDatabase}></FontAwesomeIcon></span>
+            <ProgressBar now="70" visuallyHidden />
           </div>
         </div>
       </div>
