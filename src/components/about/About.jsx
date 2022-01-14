27d49f657057cdee5104a91a758891e4b13ff1c2
gilcamiloa <cgilv6@gmail.com>
2022-01-14 01:55:59 -0500

    fixing about section parameters/containers

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <div className="about">
       <div className="aboutContainer">
-            <h1><u>HELLO</u> I am a full stack engineer based in New York City.</h1>
+            <h1><u>HELLO</u> I am a full stack engineer based in Brooklyn.</h1>
         <div className="container">
           <div className="left">
             <div className="imgContainer">
@@ -14,7 +14,9 @@ export default function About() {
           </div>
           <div className="right">
             <div className="wrapper">
-              <p>I’m a full stack engineer based in Brooklyn. My interests include construction technology suited to create large infrastructure applications. I studied full stack development in Le Wagon London and hold a Bachelor's of Science in Civil Engineering from the University of Central Florida. Currently creating projects via react.js and Ruby on Rails as well as Python. In my free time, I am an avid reader of nonfiction and a keen explorer of NYC museums.</p>
+              <p>My interests include construction technology suited to create large infrastructure applications. I studied full stack development in Le Wagon London and hold a Bachelor's of Science in Civil Engineering from the University of Central Florida. Currently creating projects via react.js and Ruby on Rails as well as learning Java and Python on the side. In my free time, I am a voracious reader and an aspiring explorer of NYC museums.</p>
+              <br />
+              <p>Feel free to reach out to me for any coding needs or collaborations.</p>
             </div>
           </div>
         </div>
