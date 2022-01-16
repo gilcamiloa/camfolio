c5559b0d1cc340af9e6faf216c5f7d0cd4a20a73
gilcamiloa <cgilv6@gmail.com>
2022-01-15 22:39:51 -0500

    fixing skills container and added a meta header icon

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -12,47 +12,44 @@ import { ProgressBar } from 'react-bootstrap'
 export default function Skills() {
   return (
     <div className="skills">
-      <div id="skills">
-          <h1>Skills</h1>
         <div className="title">
+          <h1>SKILLS</h1>
         </div>
-        <br />
-        <div className="links" >
-          <div id="progress_bar__container" className='items'>
-            <span>HTML&ensp;<FontAwesomeIcon icon={faHtml5}></FontAwesomeIcon></span>
-            <ProgressBar now="90" visuallyHidden variant="warning" />
-            <span>CSS&ensp;<FontAwesomeIcon icon={faCss3Alt}></FontAwesomeIcon></span>
-            <ProgressBar now="85" visuallyHidden variant="success" />
-            <span>JavaScript&ensp;<FontAwesomeIcon icon={faJsSquare}></FontAwesomeIcon></span>
-            <ProgressBar now="75" visuallyHidden variant="info" />
-            <span>React.js&ensp;<FontAwesomeIcon icon={faReact}></FontAwesomeIcon></span>
-            <ProgressBar now="75" visuallyHidden variant="info" />
-            <span>Python&ensp;<FontAwesomeIcon icon={faPython}></FontAwesomeIcon></span>
-            <ProgressBar now="30" visuallyHidden variant="info" />
-            <span>Ruby on Rails&ensp;<FontAwesomeIcon icon={faGem}></FontAwesomeIcon></span>
-            <ProgressBar now="85" visuallyHidden variant="danger" />
-            <span>SQL&ensp;<FontAwesomeIcon icon={faDatabase}></FontAwesomeIcon></span>
-            <ProgressBar now="70" visuallyHidden />
-            <div className="other">
-              <div className="left">
-                <h2>Other Skills</h2>
-                <h5>Heroku</h5>
-                <h5>Heroku</h5>
-                <h5>Heroku</h5>
-                <h5>Heroku</h5>
-                <h5>Heroku</h5>
-              </div>
-              <div className="right">
-                <h2>Spoken Languages</h2>
-                <h5>English (fluent)</h5>
-                <h5>Spanish (fluent)</h5>
-                <h5>Portuguese (basic)</h5>
+        <div className="container">
+          <div className="links">
+            <div id="progress_bar__container" className='items'>
+              <span>HTML&ensp;<FontAwesomeIcon icon={faHtml5}></FontAwesomeIcon></span>
+              <ProgressBar now="90" visuallyHidden variant="warning" />
+              <span>CSS&ensp;<FontAwesomeIcon icon={faCss3Alt}></FontAwesomeIcon></span>
+              <ProgressBar now="85" visuallyHidden variant="success" />
+              <span>JavaScript&ensp;<FontAwesomeIcon icon={faJsSquare}></FontAwesomeIcon></span>
+              <ProgressBar now="75" visuallyHidden variant="info" />
+              <span>React.js&ensp;<FontAwesomeIcon icon={faReact}></FontAwesomeIcon></span>
+              <ProgressBar now="75" visuallyHidden variant="info" />
+              <span>Python&ensp;<FontAwesomeIcon icon={faPython}></FontAwesomeIcon></span>
+              <ProgressBar now="30" visuallyHidden variant="info" />
+              <span>Ruby on Rails&ensp;<FontAwesomeIcon icon={faGem}></FontAwesomeIcon></span>
+              <ProgressBar now="85" visuallyHidden variant="danger" />
+              <span>SQL&ensp;<FontAwesomeIcon icon={faDatabase}></FontAwesomeIcon></span>
+              <ProgressBar now="70" visuallyHidden />
+              <div className="other">
+                <div className="left">
+                  <h2>Other Skills</h2>
+                  <h5>Heroku</h5>
+                  <h5>Heroku</h5>
+                  <h5>Heroku</h5>
+                  <h5>Heroku</h5>
+                </div>
+                <div className="right">
+                  <h2>Spoken Languages</h2>
+                  <h5>English (fluent)</h5>
+                  <h5>Spanish (fluent)</h5>
+                  <h5>Portuguese (basic)</h5>
+                </div>
               </div>
             </div>
-
           </div>
-        </div>
-      </div>
     </div>
+  </div>
   )
 }
