94408b3edaaa45f3196669b7422dd7e8d0c3f0cb
gilcamiloa <cgilv6@gmail.com>
2022-01-20 23:21:18 -0500

    changes after last merge

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -40,26 +40,6 @@ export default function Projects() {
   };
 
   return (
-    <div className="projects" id="projects">
-      <div className="container">
-        <h1>Projects</h1>
-        <div className="d-flex text-center">
-          <div className="col-12 col-lg-4 text-center">
-            <h1>Hello</h1>
-            <h2>Is there anybody in there?</h2>
-            <h2>Just nod if you can he me</h2>
-          </div>
-          <div className="col-12 col-lg-4 text-center card-category">
-            <h1>Hello</h1>
-            <h2>Is there any DATA in there?</h2>
-            <h2>Just nod if you can he me</h2>
-          </div>
-          <div className="col-12 col-lg-4 text-center">
-            <h1>Hello</h1>
-            <h2>Is there any DATA in there?</h2>
-            <h2>Just nod if you can he me</h2>
-          </div>
-
     <div className="projects">
       <div className="projectsContainer">
         <div className="title">
