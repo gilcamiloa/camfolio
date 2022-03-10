5430c05cbb3c834a9b37cbef76b3666c0a9af86e
gilcamiloa <cgilv6@gmail.com>
2022-03-09 23:06:23 -0600

    react scrolling

--- src/Sections.jsx
+++ src/Sections.jsx
@@ -1,6 +1,7 @@
 import './sections.scss'
 import { React,useState } from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
+import { Link } from 'react-scroll'
 import { useHorizontalScroll } from '../src/horizontalScroll.jsx'
 import Intro from './components/intro/Intro'
 import Projects from './components/projects/Projects'
@@ -13,16 +14,20 @@ export default function Sections() {
   const scrollRef = useHorizontalScroll();
   return (
     <div className="sections" ref={scrollRef} >
-      <Intro exact path="/" className="sectionsSlides sectionIntro" dotNav={dotNav} setDotNav={setDotNav}/>
-      <Projects path="/projects" className="sectionsSlides sectionProjects"/>
-      <Skills path="/skills" className="sectionsSlides sectionSkills"/>
-      <Contact path="/contact" className="sectionsSlides sectionContact"/>
       <div className="nav__dots first__current">
-        <Link to={<Intro />}><div id="dot1" className="dot"></div></Link>
-        <div id="dot2" className="dot" onClick={<Projects/>}></div>
-        <div id="dot3" className="dot"><Link to={<Skills />}></Link></div>
-        <Link to={<Contact />}><div id="dot4" className="dot"></div></Link>
+        {/* <Link to={<Intro/>} spy={true} smooth={true}><div id="dot2" className="dot"></div></Link> */}
+        <Link to='sectionsIntro' spy={true} smooth={true} duration={500}><div id="dot1" className="dot"></div></Link>
+        <Link to='sectionProjects' spy={true} smooth={true} duration={500}><div id="dot2" className="dot"></div></Link>
+        <Link to='sectionsSkills' spy={true} smooth={true} duration={500}><div id="dot3" className="dot"></div></Link>
+        <Link to='sectionsContact' spy={true} smooth={true} duration={500}><div id="dot4" className="dot"></div></Link>
+        {/* <Link to={<Projects/>} spy={true} smooth={true}><div id="dot2" className="dot"></div></Link>
+        <Link to={<Skills/>} spy={true} smooth={true}><div id="dot2" className="dot"></div></Link>
+        <Link to={<Contact/>} spy={true} smooth={true}><div id="dot2" className="dot"></div></Link> */}
       </div>
+      <Intro exact path="/" name='sectionsIntro' className="sectionsSlides sectionIntro" dotNav={dotNav} setDotNav={setDotNav}/>
+      <Projects path="/projects" name='sectionProjects' className="sectionsSlides sectionProjects"/>
+      <Skills path="/skills" name='sectionsSkills' className="sectionsSlides sectionSkills"/>
+      <Contact path="/contact" name='sectionsContact' className="sectionsSlides sectionContact"/>
     </div>
   );
 }
