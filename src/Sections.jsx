55b8e1522b9fa7011f3993189fcea6a6630b3c83
gilcamiloa <cgilv6@gmail.com>
2022-04-07 23:58:22 -0500

    reduce file size on images

--- src/Sections.jsx
+++ src/Sections.jsx
@@ -1,30 +1,30 @@
 import './sections.scss'
-import { React,useState } from 'react'
+import { React} from 'react'
+// import { React,useState } from 'react'
 // import { Link } from 'react-router-dom'
 import { Link } from 'react-scroll'
-import { useHorizontalScroll } from '../src/horizontalScroll.jsx'
 import Intro from './components/intro/Intro'
 import Projects from './components/projects/Projects'
 import Skills from './components/skills/Skills'
 import Contact from './components/contact/Contact'
-// import Footer from './components/footer/Footer'
 
 export default function Sections() {
-  const [dotNav, setDotNav] = useState(false);
-  const scrollRef = useHorizontalScroll();
+  var Scroll = require('react-scroll');
+  var scroll = Scroll.animateScroll;
+
+  scroll.scrollTo(0);
+  scroll.scrollToPro(100);
+  scroll.scrollToSkills(200);
+  scroll.scrollToContact(300);
   return (
-    <div className="sections" ref={scrollRef} >
+    <div className="sections" >
       <div className="nav__dots first__current">
-        {/* <Link to={<Intro/>} spy={true} smooth={true}><div id="dot2" className="dot"></div></Link> */}
-        <Link to='sectionsIntro' spy={true} smooth={true} duration={500}><div id="dot1" className="dot"></div></Link>
-        <Link to='sectionProjects' spy={true} smooth={true} duration={500}><div id="dot2" className="dot"></div></Link>
-        <Link to='sectionsSkills' spy={true} smooth={true} duration={500}><div id="dot3" className="dot"></div></Link>
-        <Link to='sectionsContact' spy={true} smooth={true} duration={500}><div id="dot4" className="dot"></div></Link>
-        {/* <Link to={<Projects/>} spy={true} smooth={true}><div id="dot2" className="dot"></div></Link>
-        <Link to={<Skills/>} spy={true} smooth={true}><div id="dot2" className="dot"></div></Link>
-        <Link to={<Contact/>} spy={true} smooth={true}><div id="dot2" className="dot"></div></Link> */}
+        <div id="dot1" className="dot"><Link to='sectionsIntro' onClick={<Intro/>} spy={true} smooth={true} duration={500} horizontal={true}></Link></div>
+        <div id="dot2" className="dot"><Link to='sectionProjects' onClick={<Projects />} spy={true} smooth={true} duration={500} horizontal={true}></Link></div>
+        <div id="dot3" className="dot"><Link to='sectionsSkills' onClick={<Skills />} spy={true} smooth={true} duration={500} horizontal={true}></Link></div>
+        <div id="dot4" className="dot"><Link to='sectionsContact' onClick={<Contact />} spy={true} smooth={true} duration={500} horizontal={true}></Link></div>
       </div>
-      <Intro exact path="/" name='sectionsIntro' className="sectionsSlides sectionIntro" dotNav={dotNav} setDotNav={setDotNav}/>
+      <Intro exact path="/" name='sectionsIntro' className="sectionsSlides sectionIntro"/>
       <Projects path="/projects" name='sectionProjects' className="sectionsSlides sectionProjects"/>
       <Skills path="/skills" name='sectionsSkills' className="sectionsSlides sectionSkills"/>
       <Contact path="/contact" name='sectionsContact' className="sectionsSlides sectionContact"/>
