2364b0c46c15c3bfd47f0182002c379e7abe5306
gilcamiloa <cgilv6@gmail.com>
2022-08-02 23:21:24 -0400

    nav links in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './app.scss';
 import React from 'react';
-// import React, { useState } from 'react';
 import Sidebar from './components/sidebar/Sidebar'
 import Intro from './components/intro/Intro'
 import Skills from './components/skills/Skills'
@@ -8,40 +7,7 @@ import About from './components/about/About'
 import Projects from './components/projects/Projects'
 import Contact from './components/contact/Contact'
 
-
 function App() {
-  // const [aboutOpen, setAboutOpen] = useState(false);
-  // const toIntro = useRef(null);
-  // const toSkills = useRef(null);
-  // const toProjects = useRef(null);
-  // const toAbout = useRef(null);
-  // const toContact = useRef(null);
-
-  // state = {
-  //   name: "",
-  // }
-
-  // handleCallback = (toIntro) => {
-  //   this.setState({ name: toIntro })
-  // }
-
-  // handleCallback = (toSkills) => {
-  //   this.setState({ name: toSkills })
-  // }
-
-  // handleCallback = (toProjects) => {
-  //   this.setState({ name: toProjects })
-  // }
-
-  // handleCallback = (toAbout) => {
-  //   this.setState({ name: toAbout })
-  // }
-
-  // handleCallback = (toContact) => {
-  //   this.setState({ name: toContact })
-  // }
-
-  // const { name } = this.state;
 
   return (
     <div className="app">
@@ -49,12 +15,12 @@ function App() {
         <div className="col-2 col-lg-2 sidebar">
           <Sidebar/>
         </div>
-        <div className="col-10 col-lg-10 sections justify-content-stretch" >
-          <Intro exact path="/" ref={(toIntro) => { this.child = toIntro; }} />
-          <Skills path="/skills" ref={(toSkills) => { this.child = toSkills; }} />
-          <Projects path="/projects" ref={(toProjects) => { this.child = toProjects; }} />
-          <About path="/about" ref={(toAbout) => { this.child = toAbout; }} />
-          <Contact path="/contact" ref={(toContact) => { this.child = toContact; }} />
+        <div className="col-10 col-lg-10 sections" >
+          <Intro exact path="/" id='#intro'/>
+          <Skills path="skills" id='#skills' />
+          <Projects path="projects" id='#projects' />
+          <About path="about" id='#about' />
+          <Contact path="contact" id='/contact' />
         </div>
     </div>
     </div>
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,95 +1,85 @@
 import './sidebar.scss'
-import React from 'react';
-import  { Link } from 'react-router-dom'
+import React from 'react'
 import logo from '../../images/cloud-images/logo2-apple-touch-icon.png'
 import ReactTooltip from 'react-tooltip'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faMailBulk } from '@fortawesome/free-solid-svg-icons'
-// import { faFile } from '@fortawesome/free-solid-svg-icons'
+// import { HashLink } from 'react-router-hash-link'
+// import Scroll from 'react-scroll';
+// const ScrollLink = Scroll.ScrollLink
+// import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
+// import Intro from '../intro/Intro'
+// import Skills from '../skills/Skills'
+// import About from '../about/About'
+// import Projects from '../projects/Projects'
+// import Contact from '../contact/Contact'
+// // import { faFile } from '@fortawesome/free-solid-svg-icons'
 
 export default function Sidebar() {
-
-  // views = [
-  //   toIntro,
-  //   toSkills,
-  //   toProjects,
-  //   toAbout,
-  //   toContact
-  // ];
-  // const toIntro = (e) => {
-  //   e.preventDefault();
-  //   window.scrollTo({
-  //     top: 0,
-  //     behavior: "smooth" });
-  // };
-
-  // const toSkills = (e) => {
-  //   window.preventDefault();
-  //   window.scrollTo({
-  //     top: 100,
-  //     behavior: "smooth" });
-  // };
-
-  // const toProjects = (e) => {
-  //   window.preventDefault();
-  //   window.scrollIntoView({
-  //     top: 200,
-  //     behavior: "smooth" });
-  // };
-
-  // const toAbout = (e) => {
-  //   window.preventDefault();
-  //   window.scrollTo({
-  //     top: 300,
-  //     behavior: "smooth" });
-  // };
-
-  // const toContact = (e) => {
-  //   window.preventDefault();
-  //   window.scrollTo({
-  //     top: 400,
-  //     behavior: "smooth" });
-  // };
-
   return (
     <div className='container'>
       <div className="sidebar_left">
         <a href='' className="logo"><img src={logo} alt='' className="logo"/></a>
       </div>
-        <div className="sidebar_router">
-        <div> <Link to='#intro'>Home</Link></div>
-        <div> <Link to='#skills'>Skills</Link></div>
-        <div> <Link to='#projects'>Projects</Link></div>
-        <div> <Link to='#about'>About</Link></div>
-        <div> <Link to='#contact'>Contact</Link></div>
-        {/* <div> <Link to={Intro}>Intro</Link></div>
-        <div> <Link to={Skills}>Skills</Link></div>
-        <div> <Link to={Projects}>Projects</Link></div>
-        <div> <Link to={About}>About</Link></div>
-        <div> <Link to={Contact}>Contact</Link></div> */}
-          {/* <NavLink component={<About />} onClick={() => setAboutOpen(!aboutOpen)} className='active'>ABOUT</NavLink> */}
+      <div className="sidebar_router">
+        {/* <div><HashLink to={<Intro/>} spy={true} smooth={true} offset={50} duration={500}>Intro</HashLink></div>
+        <div><HashLink to={<Skills />} spy={true} smooth={true} offset={50} duration={500}>Skills</HashLink></div>
+        <div><HashLink to={<Intro />} spy={true} smooth={true} offset={50} duration={500}>Projects</HashLink></div>
+        <div><HashLink to={<Intro />} spy={true} smooth={true} offset={50} duration={500}>About</HashLink></div>
+        <div><HashLink to={<Intro />} spy={true} smooth={true} offset={50} duration={500}>Contact</HashLink></div> */}
+      <div>
+        <div
+          style={{"cursor":"pointer"}}
+          onClick={() => {
+            const intro = document.querySelector('#intro')
+            intro.scrollIntoView({ top:0,behavior: 'smooth', block: 'center' })
+          }}>
+              <span>Intro</span>
         </div>
-        <div className="sidebar_links">
-          <div className='link__wrapper'>
-            <a href="https://www.linkedin.com/in/camilo-gil" target="_blank" rel="noopener noreferrer" id='a-linkedin' data-tip data-for="linkedin">
-              <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
-              <ReactTooltip id="linkedin" place="top" effect="solid" backgroundColor='none' data-delay-show='700'>LinkedIn</ReactTooltip>
-            </a>
-            <a href="https://www.github.com/gilcamiloa" target="_blank" rel="noopener noreferrer" id='a-github' data-tip data-for="github">
-              <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
-              <ReactTooltip id="github" place="top" effect="solid" backgroundColor='none' data-delay-show='1200'>GitHub</ReactTooltip>
-            </a>
-            <a href="mailto:cgilv6@gmail.com" target="_blank" rel="noopener noreferrer" id='a-email' data-tip data-for="email">
-              <FontAwesomeIcon icon={faMailBulk}></FontAwesomeIcon>
-              <ReactTooltip id="email" place="top" effect="solid" backgroundColor='none' data-delay-show='700'>Email</ReactTooltip>
-            </a>
-            {/* <a href="https://drive.google.com/file/d/1WRmISIuZONc6EHOZwdzkxUKru2B4K0RY/view?usp=sharing" target="_blank" rel="noreferrer" id='a-resume' data-tip data-for="resume">
-          <FontAwesomeIcon icon={faFile}></FontAwesomeIcon>
-          <ReactTooltip id="resume" place="top" effect="solid" backgroundColor='none' data-delay-show='700'>Resume / CV</ReactTooltip>
+        <div
+          style={{"cursor":"pointer"}}
+          onClick={() => {
+            const skills = document.querySelector('#skills')
+            skills.scrollIntoView({ top: 100,behavior: 'smooth', block: 'center' })
+          }}>
+              <span>Skills</span>
+        </div>
+
+        {/* <a href='#skills'>
+            <span>Intro</span>
+        </a>
+        <a href='#projects'>
+            <span>Intro</span>
+        </a>
+        <a href='#intro'>
+            <span>Intro</span>
+        </a>
+        <a href='#intro'>
+            <span>Intro</span>
         </a> */}
-          </div>
+      </div>
+      </div>
+      <div className="sidebar_links">
+        <div className='link__wrapper'>
+          <a href="https://www.linkedin.com/in/camilo-gil" target="_blank" rel="noopener noreferrer" id='a-linkedin' data-tip data-for="linkedin">
+            <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
+            <ReactTooltip id="linkedin" place="top" effect="solid" backgroundColor='none' data-delay-show='700'>LinkedIn</ReactTooltip>
+          </a>
+          <a href="https://www.github.com/gilcamiloa" target="_blank" rel="noopener noreferrer" id='a-github' data-tip data-for="github">
+            <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
+            <ReactTooltip id="github" place="top" effect="solid" backgroundColor='none' data-delay-show='1200'>GitHub</ReactTooltip>
+          </a>
+          <a href="mailto:cgilv6@gmail.com" target="_blank" rel="noopener noreferrer" id='a-email' data-tip data-for="email">
+            <FontAwesomeIcon icon={faMailBulk}></FontAwesomeIcon>
+            <ReactTooltip id="email" place="top" effect="solid" backgroundColor='none' data-delay-show='700'>Email</ReactTooltip>
+          </a>
+          {/* <a href="https://drive.google.com/file/d/1WRmISIuZONc6EHOZwdzkxUKru2B4K0RY/view?usp=sharing" target="_blank" rel="noreferrer" id='a-resume' data-tip data-for="resume">
+        <FontAwesomeIcon icon={faFile}></FontAwesomeIcon>
+        <ReactTooltip id="resume" place="top" effect="solid" backgroundColor='none' data-delay-show='700'>Resume / CV</ReactTooltip>
+      </a> */}
         </div>
       </div>
+    </div>
   );
 }
