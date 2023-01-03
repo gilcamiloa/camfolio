c54e987061e771781464bcc8ba9b3d8f3e8f706d
gilcamiloa <cgilv6@gmail.com>
2023-01-03 14:09:20 -0500

    camfolio 1/3: sidebar changes

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -9,33 +9,41 @@ export default function Sidebar() {
   return (
     <div className='container'>
       <div className="sidebar_logo">
-        <a href='#intro' className="a_logo"><img src="https://res.cloudinary.com/cgilv6/image/upload/v1664395399/camfolio/cloud-images/logo2-apple-touch-icon_xxcod7.png" alt='' className="img_logo"/></a>
+        <a href='#intro'>
+          <img className="img_logo" src="https://res.cloudinary.com/cgilv6/image/upload/v1672689608/camfolio/cloud-images/logo2-apple-touch-icon_xxcod7_suo63v.svg" alt=''/>
+        </a>
       </div>
         <div className="sidebar_router">
-            <a href='#skills'>
+            <a href='#skills' className='sidebar-routes'>
               <span>Skills</span>
             </a>
-            <a href='#projects'>
+            <a href='#projects' className='sidebar-routes'>
               <span>Projects</span>
             </a>
-            <a href='#about'>
+            <a href='#about' className='sidebar-routes'>
               <span>About</span>
             </a>
-            <a href='#contact'>
+            <a href='#contact' className='sidebar-routes'>
               <span>Contact</span>
             </a>
         </div>
       <div className="sidebar_links">
         <div className='link__wrapper'>
-          <a href="https://www.linkedin.com/in/camilo-gil" target="_blank" rel="noopener noreferrer" id='a-linkedin' data-tip data-for="tip-linkedin">
+          <a  href="https://www.linkedin.com/in/camilo-gil"
+              target="_blank" rel="noopener noreferrer"
+              id='a-linkedin' data-tip data-for="tip-linkedin">
             <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
             <ReactTooltip id="tip-linkedin" place="top" effect="solid" backgroundColor='none' data-delay-show='700'>LinkedIn</ReactTooltip>
           </a>
-          <a href="https://www.github.com/gilcamiloa" target="_blank" rel="noopener noreferrer" id='a-github' data-tip data-for="tip-github">
+          <a  href="https://www.github.com/gilcamiloa"
+              target="_blank" rel="noopener noreferrer"
+              id='a-github' data-tip data-for="tip-github">
             <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
             <ReactTooltip id="tip-github" place="top" effect="solid" backgroundColor='none' data-delay-show='1200'>GitHub</ReactTooltip>
           </a>
-          <a href="mailto:cgilv6@gmail.com" target="_blank" rel="noopener noreferrer" id='a-email' data-tip data-for="tip-email">
+          <a  href="mailto:cgilv6@gmail.com"
+              target="_blank" rel="noopener noreferrer"
+              id='a-email' data-tip data-for="tip-email">
             <FontAwesomeIcon icon={faMailBulk}></FontAwesomeIcon>
             <ReactTooltip id="tip-email" place="top" effect="solid" backgroundColor='none' data-delay-show='700'>Email</ReactTooltip>
           </a>
