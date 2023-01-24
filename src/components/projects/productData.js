09fc85f70fbc8d3c57a2f3390e6487a12c4ff2a0
gilcamiloa <cgilv6@gmail.com>
2023-01-24 02:31:19 -0500

    responsiveness and projects

--- src/components/projects/productData.js
+++ src/components/projects/productData.js
@@ -20,13 +20,13 @@ export const products = [
     link: "http://github.com/gilcamiloa/thegreenroom",
     desc: "This collab simulates up and coming musical acts being able to book performing venues directly with owners avoiding the need for third party management."
   },
-  // {
-  //   id: 4,
-  //   name:'Leetcode Progress',
-  //   img: "https://res.cloudinary.com/cgilv6/image/upload/v1661282262/camfolio/projects-images/cam-leetcode_jqrwzb.png",
-  //   link: "https://github.com/gilcamiloa/LeetCodePush",
-  //   desc: "View my Leetcode progress on Github! Languages used: JavaScript, Python, C++ among others.",
-  // },
+  {
+    id: 6,
+    name:'Leetcode Progress',
+    img: "https://res.cloudinary.com/cgilv6/image/upload/v1661282262/camfolio/projects-images/cam-leetcode_jqrwzb.png",
+    link: "https://github.com/gilcamiloa/LeetCodePush",
+    desc: "View my Leetcode progress on Github! Languages used: JavaScript, Python, C++ among others.",
+  },
   {
     id: 4,
     name: 'Minesweeper Clone',
@@ -41,18 +41,18 @@ export const products = [
     link: "https://github.com/gilcamiloa/thegreenroom",
     desc: "This collab simulates user rentals for private lavatory facilities within a specified city radius built using a built-in map API to select desired preference.",
   },
-  {
-    id: 6,
-    name: 'Intro to C++',
-    img: "https://res.cloudinary.com/cgilv6/image/upload/v1666409440/camfolio/projects-images/intro-to-c_pb6xl8.png",
-    link: "https://drive.google.com/drive/folders/1E9c0XEPNdt2zjIozjI7LTCibUj_4FJ1i?usp=sharing",
-    desc: "Intro to C++ course projects created and compiled using CLion IDE."
-  },
+  // {
+  //   id: 6,
+  //   name: 'Intro to C++',
+  //   img: "https://res.cloudinary.com/cgilv6/image/upload/v1666409440/camfolio/projects-images/intro-to-c_pb6xl8.png",
+  //   link: "https://drive.google.com/drive/folders/1E9c0XEPNdt2zjIozjI7LTCibUj_4FJ1i?usp=sharing",
+  //   desc: "Intro to C++ course projects created and compiled using CLion IDE."
+  // },
   // {
   //   id: 7,
   //   name: 'stackatto',
   //   img: "https://res.cloudinary.com/cgilv6/image/upload/v1643858078/camfolio/projects-images/camfolio-skills_ondfoi.png",
   //   link: "",
-  //   desc: "Stackatto, founded in 2022, is a website that allows users to receive anonymous feedback on their musical performance clips from other users. <br>Users can either pay for the service or earn credit by rating others' submissions. The site is broken down into begineer, intermediate, and advanced groups via a complex algorithm allowing a safe space for all users. All users are rated for different qualities."
+  //   desc: "Stackatto, founded in 2022, is a website that allows users to receive anonymous feedback on their musical performance clips from other users. <br>Users can either pay for the service or earn credit by rating others' submissions. The site is broken down into beginner, intermediate, and advanced groups via a complex algorithm allowing a safe space for all users. All users are rated for different qualities."
   // },
 ];
